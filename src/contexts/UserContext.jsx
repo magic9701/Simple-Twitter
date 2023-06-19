3482fac3ebe65f512539f7b8053d4485c024b88d
magic9701 <magic9701@gmail.com>
2023-06-19 23:46:21 +0800

    Fix : 刪除多餘程式碼

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react'
+import { createContext } from 'react'
 import { followUser, unfollowUser } from 'api/followship'
 import { likeTweet, unlikeTweet } from 'api/PostTweet'
 
